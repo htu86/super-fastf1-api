23ddc1485ceb71e7ce27e7ed713236dc28ced42d
enrc <enrico.cortelazzi17@gmail.com>
2025-03-30 23:16:19 +0200

    Add: seperate file for the car telemetry endpoint

---
+++ routes/carTelemetry.py
@@ -0,0 +1,99 @@
+from fastapi import HTTPException
+import fastf1
+
+def getDriverLapData(year: int, gp: str, session_type: str, driver: str, lap_number: int):
+  # Endpoint to retrieve car data and lap data for a specific driver and lap number.
+
+  # fastf1.Cache.enable_cache("./cache")  # Enable caching for better performance
+  raceSession = fastf1.get_session(year, gp, session_type)
+  raceSession.load(telemetry=True)
+
+  driverData = raceSession.laps.pick_drivers(driver).pick_laps(lap_number)
+  if driverData.empty:
+    raise HTTPException(status_code=404, detail="Driver not found or no lap data available.")
+
+  car_data = driverData.get_car_data()
+  lap_data = driverData.to_dict(orient="list")  # Convert lap data to a JSON-serializable dictionary
+  car_data_dict = car_data.to_dict(orient="list")  # Convert car data to a JSON-serializable dictionary
+
+
+  return {
+    "track": raceSession.event['EventName'],
+    "compound":driverData["Compound"].iloc[0],
+    "lap_data": lap_data,
+    "car_data": car_data_dict
+  }
+
+def getFastestSessionLap(year: int, gp: str, session_type: str):
+  # Endpoint to retrieve the fastest lap info in a session.
+
+  # fastf1.Cache.enable_cache("./cache")  # Enable caching for better performance
+  try:
+    raceSession = fastf1.get_session(year, gp, session_type)
+    raceSession.load()
+  except Exception as e:
+    raise HTTPException(status_code=400, detail=f"Error loading session: {str(e)}")
+
+  if raceSession.event['EventName'] is None:
+    raise HTTPException(status_code=404, detail="Grand Prix not found.")
+
+  fastestSessionLap = raceSession.laps.pick_fastest()
+  if fastestSessionLap.empty:
+    raise HTTPException(status_code=404, detail="No lap data available for the session.")
+
+  carData = fastestSessionLap.get_car_data().to_dict(orient="list")
+
+  return {
+    "track": raceSession.event['EventName'],
+    "driver": fastestSessionLap['Driver'],
+    "lap_time": str(fastestSessionLap['LapTime']),
+    "lap_number": int(fastestSessionLap['LapNumber']),
+    "team": fastestSessionLap['Team'],
+    "compound": fastestSessionLap.laps["Compound"].iloc[0],
+    "car_data": carData
+  }
+
+def getDriverPersonalBestLap(year: int, gp: str | int, session_type: str, driver: str):
+    # Endpoint to retrieve the personal best lap info for a specific driver in a session.
+
+    # fastf1.Cache.enable_cache("./cache")  # Enable caching for better performance
+  raceSession = fastf1.get_session(year, gp, session_type)
+  raceSession.load()
+
+  driverData = raceSession.laps.pick_drivers(driver)
+  if driverData.empty:
+    raise HTTPException(status_code=404, detail="Driver not found or no lap data available.")
+
+  driverBestLap = driverData.pick_fastest()
+  if driverBestLap.empty:
+    raise HTTPException(status_code=404, detail="No lap data available for the driver.")
+
+  personal_best_lap_car_data = driverBestLap.get_car_data().to_dict(orient="list")
+  if driverBestLap.empty:
+    raise HTTPException(status_code=404, detail="No lap data available for the session.")
+  
+  return {
+    "driver": driverBestLap['Driver'],
+    "lap_time": str(driverBestLap['LapTime']),
+    "lap_number": int(driverBestLap['LapNumber']),
+    "team": driverBestLap['Team'],
+    "car_data": personal_best_lap_car_data
+}
+
+def getDriverLaps(year: int, gp: str, session_type: str, driver: str):
+  
+  raceSession = fastf1.get_session(year, gp, session_type)
+  raceSession.load()
+  
+  driverLapInfo = raceSession.laps.pick_drivers(driver)
+  if driverLapInfo.empty:
+    raise HTTPException(status_code=404, detail="Driver not found or no lap data available.")
+
+  # Replace NaN, Infinity, and -Infinity with None in the DataFrame
+  driverLapInfo = driverLapInfo.replace([float('inf'), float('-inf')], None).fillna(value="None")
+
+  # Convert lap data to a JSON-serializable dictionary
+  formattedDriverLapInfo = driverLapInfo.to_dict(orient="list")
+  return {
+    "laps": formattedDriverLapInfo
+  }
