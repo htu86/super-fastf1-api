21e8098eefe32c3e807e5571aa2fbcce9b12e025
enrc <enrico.cortelazzi17@gmail.com>
2025-04-02 11:28:23 +0200

    Add: API endpoints for newly added files

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 from fastapi import FastAPI
-from routes.carTelemetry import (
+from routes.lapInfo import (
   getDriverLapData,
-  getFastestSessionLap,
-  getDriverPersonalBestLap,
   getDriverLaps
 )
-from routes.seasonInfo import (
+from routes.sessionInfo import (
   getSessioninfo,
-  getSessionWeatherData  
+  getSessionWeatherData , 
+  getDriversInSession,
+  getSessionTimingData
+)
+from routes.lapRecords import(
+  getDriverPersonalBestLap,
+  getFastestSessionLap
+)
+
+from routes.trackInfo import(
+  getCircuitInformation,
+  getTrackStatusHistory  
 )
 
 app = FastAPI()
 
-# Car telemetry
+# Endpoints related to lap data
 
 @app.get("/driver-lap-telemetry/{year}/{gp}/{session_type}/{driver}/{lap_number}")
 def driver_lap_telemetry(year: int, gp: str, session_type: str, driver: str, lap_number: int):
@@ -30,12 +39,31 @@ def personal_best_lap_telemetry(year: int, gp: str, session_type: str, driver: s
 def driver_laps(year: int, gp: str, session_type: str, driver: str):
   return getDriverLaps(year, gp, session_type, driver)
 
-# Session info
+# Endpoints related to session information
 
 @app.get("/session-info/{year}/{gp}/{session_type}")
 def session_info(year: int, gp: str, session_type: str):
   return getSessioninfo(year, gp, session_type)
 
 @app.get("/session-weather/{year}/{gp}/{session_type}")
-def session_weather(year:int, gp: str, session_type: str):
+def session_weather(year: int, gp: str, session_type: str):
   return getSessionWeatherData(year, gp, session_type)
+
+@app.get("/session-drivers/{year}/{gp}/{session_type}")
+def session_drivers(year: int, gp: str, session_type: str):
+  return getDriversInSession(year, gp, session_type)  
+
+@app.get("/session-time/{year}/{gp}/{session_type}")
+def session_time(year: int, gp: str, session_type: str):
+  return getSessionTimingData(year, gp, session_type)  
+
+
+# Endpoints related to track information
+
+@app.get("/circuit-geographic-data/{year}/{gp}/{session_type}")
+def circuit_info(year: int, gp: str, session_type: str):
+  return getCircuitInformation(year, gp, session_type)
+
+@app.get("/track-status-history/{year}/{gp}/{session_type}")
+def track_status_history(year: int, gp: str, session_type: str):
+  return getTrackStatusHistory(year, gp, session_type)
