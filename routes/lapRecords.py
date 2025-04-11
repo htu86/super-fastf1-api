a8ed4d63ab7b269ebbcce8e97a27914a215f94cc
enrc <enrico.cortelazzi17@gmail.com>
2025-04-11 21:29:08 +0200

    Fix: Refactor and fix AttributeError

--- routes/lapRecords.py
+++ routes/lapRecords.py
@@ -1,62 +1,62 @@
-import fastf1
 from fastapi import HTTPException
 from utils.sessionLoad import loadSession
 from utils.validateInput import validateInputs
 
-def getFastestSessionLap(year: int, granPrix: str, sessionType: str):
-  # Endpoint to retrieve the fastest lap info in a session.
+def get_fastest_session_lap(year: int, gran_prix: str, session_type: str):
+    # Endpoint to retrieve the fastest lap info in a session.
 
-  validateInputs(year, granPrix , sessionType )
+    validateInputs(year, gran_prix, session_type)
+    try:
+      race_session = loadSession(year, gran_prix, session_type)
+    except Exception as e:
+      raise HTTPException(status_code=500, detail=f"Failed to load session: {str(e)}")
+
+    if race_session.event['EventName'] is None:
+      raise HTTPException(status_code=404, detail="Grand Prix not found.")
+
+    fastest_session_lap = race_session.laps.pick_fastest()
+    if fastest_session_lap.empty:
+      raise HTTPException(status_code=404, detail="No lap data available for the session.")
+
+    car_data = fastest_session_lap.get_car_data().to_dict(orient="list")
+
+    event_name = race_session.event['EventName']
+
+    return {
+      "track": event_name,  # Use event_name instead of fastest_session_lap.event
+      "driver": fastest_session_lap['Driver'],
+      "lap_time": str(fastest_session_lap['LapTime']),
+      "lap_number": int(fastest_session_lap['LapNumber']),
+      "team": fastest_session_lap['Team'],
+      "compound": fastest_session_lap['Compound'],  # Fixed access to compound
+      "car_data": car_data
+    }
+
+def get_driver_personal_best_lap(year: int, gran_prix: str, session_type: str, driver: str):
+
+  validateInputs(year, gran_prix , session_type )
   try:
-    raceSession = loadSession(year, granPrix, sessionType)
+    race_session = loadSession(year, gran_prix, session_type)
   except Exception as e:
     raise HTTPException(status_code=500, detail=f"Failed to load session: {str(e)}")
   
 
-  if raceSession.event['EventName'] is None:
-    raise HTTPException(status_code=404, detail="Grand Prix not found.")
-
-  fastestSessionLap = raceSession.laps.pick_fastest()
-  if fastestSessionLap.empty:
-    raise HTTPException(status_code=404, detail="No lap data available for the session.")
-
-  carData = fastestSessionLap.get_car_data().to_dict(orient="list")
-
-  return {
-    "track": raceSession.event['EventName'],
-    "driver": fastestSessionLap['Driver'],
-    "lap_time": str(fastestSessionLap['LapTime']),
-    "lap_number": int(fastestSessionLap['LapNumber']),
-    "team": fastestSessionLap['Team'],
-    "compound": fastestSessionLap.laps["Compound"].iloc[0],
-    "car_data": carData
-  }
-
-def getDriverPersonalBestLap(year: int, granPrix: str, sessionType: str, driver: str):
-
-  validateInputs(year, granPrix , sessionType )
-  try:
-    raceSession = loadSession(year, granPrix, sessionType)
-  except Exception as e:
-    raise HTTPException(status_code=500, detail=f"Failed to load session: {str(e)}")
-  
-
-  driverData = raceSession.laps.pick_drivers(driver)
-  if driverData.empty:
+  driver_data = race_session.laps.pick_drivers(driver)
+  if driver_data.empty:
     raise HTTPException(status_code=404, detail="Driver not found or no lap data available.")
 
-  driverBestLap = driverData.pick_fastest()
-  if driverBestLap.empty:
+  driver_best_lap = driver_data.pick_fastest()
+  if driver_best_lap.empty:
     raise HTTPException(status_code=404, detail="No lap data available for the driver.")
 
-  bestLapCarData = driverBestLap.get_car_data()
-  if driverBestLap.empty:
+  car_data = driver_best_lap.get_car_data()
+  if driver_best_lap.empty:
     raise HTTPException(status_code=404, detail="No lap data available for the session.")
   
   return {
-    "driver": driverBestLap['Driver'],
-    "lap_time": str(driverBestLap['LapTime']),
-    "lap_number": int(driverBestLap['LapNumber']),
-    "team": driverBestLap['Team'],
-    "car_data": bestLapCarData.to_dict(orient="list")
+    "driver": driver_best_lap['Driver'],
+    "lap_time": str(driver_best_lap['LapTime']),
+    "lap_number": int(driver_best_lap['LapNumber']),
+    "team": driver_best_lap['Team'],
+    "car_data": car_data.to_dict(orient="list")
 }
